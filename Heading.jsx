019b1fb10a43acb9dab24d9c9835b9806dd5338c
Al-Amin <37735466+thealamindev@users.noreply.github.com>
2023-11-30 14:31:29 +0600

    Add files via upload

---
+++ Heading.jsx
@@ -0,0 +1,25 @@
+// =======Single Color Span ======
+
+import React from "react";
+
+const Heading = ({ text }) => {
+  let amin = text.replace("#", "<span class='one'>").replace("#", "</span>");
+
+  return <h1 dangerouslySetInnerHTML={{ __html: amin }}></h1>;
+};
+
+export default Heading;
+
+// =======Single Color Span End======
+
+// =======Multiple Color Span With Rejex
+import React from "react";
+
+function Heading({ text }) {
+    let pattern = /#([^#]+)#/g;
+    let textWithSpans = text.replace(pattern,"<span class='one'>$1</span>");
+    return <h1 dangerouslySetInnerHTML={{ __html: textWithSpans }} />;
+}
+
+export default Heading;
+// =======Multiple Color Span End 
